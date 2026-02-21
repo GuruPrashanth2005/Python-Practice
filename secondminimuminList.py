010b433b2e87b348188c732bf93725f96b77163c
Guru Prashanth S <gprashanth0905@gmail.com>
2026-02-21 20:12:25 +0530

    Modified

--- secondminimuminList.py
+++ secondminimuminList.py
@@ -1,21 +1,21 @@
-# n=list(map(int,input("enter numbers:").split()))
-# def sec_min(n):
-#     min1=float('inf')
-#     min2=float('inf')
-#     for i in n:
-#         if i<min1:
-#             min2=min1
-#             min1=i
-#         elif i<min2 and i!=min1:
-#             min2=i
-#     if min2==float('inf'):
-#         return None
-#     return min2
-# result=sec_min(n)
-# if result is None:
-#     print("no second value")
-# else:
-#     print(f"second value:{result}")
+n=list(map(int,input("enter numbers:").split()))
+def sec_min(n):
+    min1=float('inf')
+    min2=float('inf')
+    for i in n:
+        if i<min1:
+            min2=min1
+            min1=i
+        elif i<min2 and i!=min1:
+            min2=i
+    if min2==float('inf'):
+        return None
+    return min2
+result=sec_min(n)
+if result is None:
+    print("no second value")
+else:
+    print(f"second value:{result}")
 
 n=int(input("enter no of elements:"))
 lst=[]
